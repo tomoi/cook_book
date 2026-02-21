d8c6e98e887cb1fa8ed62cc2b3223d6a17f5bb04
Tomas Godoybaca <tommygodoybaca@outlook.com>
2026-02-20 21:16:34 -0600

    updated page to use server action

--- src/app/(pages)/recipeinput/page.tsx
+++ src/app/(pages)/recipeinput/page.tsx
@@ -1,11 +1,11 @@
 'use server';
 
-import { useSignedInStatus } from '@/app/hooks/_useSignedInStatus';
 import { redirect } from 'next/navigation';
 import RecipeForm from './recipeForm/RecipeForm';
+import { getUserSession } from '@/app/actions/auth';
 
 export default async function RecipeInput() {
-    const userSession = await useSignedInStatus();
+    const userSession = await getUserSession();
     //redirect user home if they are not logged in.
     if (userSession === null) {
         redirect('/');
